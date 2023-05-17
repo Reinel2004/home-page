0fbd658fd9d100520d2dafbc06a75f96bdb8bd12
Reinel Gueco <venvengueco@gmail.com>
2023-05-17 11:42:41 +0800

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-const observer = new IntersectionObserver((entries) =>{
+const the_animation = document.querySelectorAll('.animation')
+
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
         }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+   
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
 
-    });
-});
+    observer.observe(elements);
+  } 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
